5b62f6eeef3c0a18e5ce41dc370ec7f4f0a8f578
drissboumlik <idrissboumlik@gmail.com>
2021-10-04 15:23:58 +0100

    Refactor play cmd url handling

--- imports/audio_activity.py
+++ imports/audio_activity.py
@@ -1,5 +1,5 @@
 from setup.properties import *
-
+import re
 
 def init_audio_activity(params):
 
@@ -16,19 +16,25 @@ def init_audio_activity(params):
 
 	# @client.command(pass_context=True)
 	@slash.slash(name = "play", description = "Play a Youtube url", guild_ids = [guildId])
-	async def play(ctx, url = None, volume: int = 10):
+	async def play(ctx, url):
 
 		try:
 			user = ctx.author
 			voice = get(client.voice_clients, guild = ctx.guild)
 			
 			if voice == None:
-				
-				if (url == None):
-					await ctx.send('You need to provide a valide youtube url')
-					return
+
 				if (user.voice == None):
 					await ctx.send('You need to be connected to a voice channel')
+					return
+				
+				youtube_regex = (
+					r'(https?://)?(www\.)?'
+					'(youtube|youtu|youtube-nocookie)\.(com|be)/'
+					'(watch\?v=|embed/|v/|.+\?v=)?([^&=%\?]{11})')
+				youtube_regex_match = re.match(youtube_regex, url)
+				if not youtube_regex_match:
+					await ctx.send('You need to provide a valide youtube url')
 					return
 				vc = user.voice.channel
 
@@ -39,25 +45,17 @@ def init_audio_activity(params):
 					with YoutubeDL(ydl_opts) as ydl:
 						info = ydl.extract_info(url, download = False)
 					URL = info['formats'][0]['url']
-					# voice.volume = 0 #Not working
 
-					# voice.source.volume = 0.1
-					# await ctx.send(f"Joining ...")
 
 					await ctx.send(f':musical_note: **Now playing :** <{url}>')
 					voice.play(FFmpegPCMAudio(URL, **FFMPEG_OPTIONS))
-					# voice.is_playing()
 					
 					voice.source = discord.PCMVolumeTransformer(voice.source)
-					# voice.source.volume = float(volume)
 
-					try:
-						voice.source.volume = volume/100
-					except:
-						voice.source.volume = 0.1
 			else:
 				await ctx.send("I'm already connected!")
 		except Exception as ex:
+			print('----- /play -----')
 			print(ex)
 
 	# @client.command(pass_context=True)
@@ -65,12 +63,13 @@ def init_audio_activity(params):
 	async def pause(ctx):
 		try:
 			voice = get(client.voice_clients, guild = ctx.guild)
-			if voice.is_playing():
-				await ctx.send('Pausing ...')
+			if voice and voice.is_playing():
+				await ctx.send('⏸ Pausing ...')
 				voice.pause()
 			else:
 				await ctx.send('❌ The bot is not playing anything at the moment')
 		except Exception as ex:
+			print('----- /pause -----')
 			print(ex)
 			
 	# @client.command(pass_context=True)
@@ -78,12 +77,13 @@ def init_audio_activity(params):
 	async def resume(ctx):
 		try:
 			voice = get(client.voice_clients, guild = ctx.guild)
-			if voice.is_paused():
-				await ctx.send('Resuming ...')
+			if voice and voice.is_paused():
+				await ctx.send('▶ Resuming ...')
 				voice.resume()
 			else:
-				await ctx.send('❌ The bot was not playing anything before this')
+				await ctx.send('❌ The bot is already (or not) playing something before this')
 		except Exception as ex:
+			print('----- /resume -----')
 			print(ex)
 	
 	# @client.command(pass_context=True)
@@ -91,21 +91,24 @@ def init_audio_activity(params):
 	async def stop(ctx):
 		try:
 			voice = get(client.voice_clients, guild = ctx.guild)
-			if voice.is_playing():
-				await ctx.send('Stopping ...')
+			if voice and voice.is_playing():
+				await ctx.send('⏹ Stopping ...')
 				voice.stop()
+				if voice != None:
+					await voice.disconnect()
 			else:
-				await ctx.send('The bot is not playing anything at the moment')
+				await ctx.send('❌ The bot is not playing anything at the moment')
 		except Exception as ex:
+			print('----- /stop -----')
 			print(ex)
 
-	# @client.command(pass_context=True)
-	@slash.slash(name = "leave", description = "Disconnect the bot from the voice room", guild_ids = [guildId])
-	async def leave(ctx):
-		try:
-			await ctx.send('Leaving ...')
-			voice = get(client.voice_clients, guild = ctx.guild)
-			if voice != None:
-				await voice.disconnect()
-		except Exception as ex:
-			print(ex)
+	# # @client.command(pass_context=True)
+	# @slash.slash(name = "leave", description = "Disconnect the bot from the voice room", guild_ids = [guildId])
+	# async def leave(ctx):
+	# 	try:
+	# 		await ctx.send('Leaving ...')
+	# 		voice = get(client.voice_clients, guild = ctx.guild)
+	# 		if voice != None:
+	# 			await voice.disconnect()
+	# 	except Exception as ex:
+	# 		print(ex)
