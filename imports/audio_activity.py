095adf7afc52db5dd8eff7a54d2e018ba2045109
drissboumlik <idrissboumlik@gmail.com>
2021-10-16 00:53:04 +0100

    Display playlist + Refactor audio activity code

--- imports/audio_activity.py
+++ imports/audio_activity.py
@@ -11,16 +11,21 @@ def init_audio_activity(params):
 	FFmpegPCMAudio = params['FFmpegPCMAudio']
 	tasks = params['tasks']
 	playlist = []
-	currentTrackIndex = -1
+	currentTrackIndex = 0
 
 
-	ydl_opts = {'format': 'bestaudio/best'}
+	ydl_opts = {'noplaylist': True,
+        'outtmpl': 'music',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '128',
+        }],'format': 'bestaudio/best'}
 	FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
 
 	######################## PLAY ########################
 	@slash.slash(name = "play", description = "Play a Youtube url", guild_ids = [guildId])
 	async def play(ctx, url):
-
 		try:
 			nonlocal currentTrackIndex, playlist, ydl_opts
 			user = ctx.author
@@ -41,107 +46,151 @@ def init_audio_activity(params):
 
 			vc = user.voice.channel
 
-			await ctx.send(f':arrow_down: **Loading :** <{url}>')
+			# await ctx.send(f'⬇ **Loading ...**')
+
+			if not url:
+				if not len(playlist):
+					await ctx.send('The playlist is empty ⚠')
+					return
+				playTrack(ctx)
+				return
 
 			if (voice):
 				if (voice.is_connected()):
 					if (voice.is_playing()):
-						playlist.append(url)
-						await ctx.send("1 Track is queued")
+						await ctx.send("⬆ Track is queued")
+						track = extrackUrlData(url)
+						playlist.append(track)
 					else:
-						await resetPlayer(ctx, ydl_opts, url, "2 Track is queued + played")
+						await resetPlayer(ctx, url, "🎵 Track is queued + played")
 				else:
-					await resetPlayer(ctx, ydl_opts, url, "3 Track is queued + played", vc)
+					await resetPlayer(ctx, url, "🎵 Track is queued + played", vc)
 			else:
-				await resetPlayer(ctx, ydl_opts, url, "4 connect + Track is queued + played", vc)
+				await resetPlayer(ctx, url, "🔌 connect + Track is queued + played", vc)
 		except Exception as ex:
 			print('----- /play -----')
 			print(ex)
 
-	async def resetPlayer(ctx, ydl_opts, url, msg, vc=None):
+	def extrackUrlData(url):
+		nonlocal ydl_opts
+		with YoutubeDL(ydl_opts) as ydl:
+			info = ydl.extract_info(url, download = False)
+		URL = info['formats'][0]['url']
+		title = info['title']
+		duration = info['duration']
+		return {"_url": URL, "url": url, "title": title, "duration": duration}
+
+	async def resetPlayer(ctx, url, msg, vc=None):
 		try:
-			nonlocal currentTrackIndex, playlist
+			nonlocal currentTrackIndex, playlist, ydl_opts
+			await ctx.send(msg)
 			playlist = []
-			playlist.append(url)
+			track = extrackUrlData(url)
+			playlist.append(track)
 			currentTrackIndex = 0
 			if vc:
 				await vc.connect()
-			voice = get(client.voice_clients, guild = ctx.guild)
-			await playTrack(ctx, currentTrackIndex, voice, ydl_opts)
-			await ctx.send(msg)
+			await playTrack(ctx)
 		except Exception as ex:
 			print('----- resetPlayer -----')
 			print(ex)
 		
 
-	async def playTrack(ctx, index, voice, ydl_opts):
+	async def playTrack(ctx):
 		try:
-			nonlocal currentTrackIndex, playlist
+			nonlocal currentTrackIndex, playlist, ydl_opts
 
-			print(f'index : {index}')
+			print(f'index : {currentTrackIndex}')
 			print(f'length : {len(playlist)}')
 
-			url = playlist[index]
-			with YoutubeDL(ydl_opts) as ydl:
-				info = ydl.extract_info(url, download = False)
-			URL = info['formats'][0]['url']
-			await ctx.send(f':musical_note: **Now playing :** <{url}>')
-			voice.play(FFmpegPCMAudio(URL, **FFMPEG_OPTIONS))
+			track = playlist[currentTrackIndex]
+			
+			await ctx.send(f'🎵 **Now playing :** __{track["title"]}__')
+			voice = get(client.voice_clients, guild = ctx.guild)
+			voice.play(FFmpegPCMAudio(track['_url'], **FFMPEG_OPTIONS))
 
-			if index == len(playlist) - 1:
-				duration = info['duration']
-				@tasks.loop(seconds=duration, count=2, reconnect=True)
-				async def toNextTrack():
-					try:
-						nonlocal currentTrackIndex, playlist
-						if toNextTrack.current_loop != 0:
-							currentTrackIndex = currentTrackIndex + 1
-							voice.stop()
-							await playTrack(ctx, currentTrackIndex, voice, ydl_opts)
-					except Exception as ex:
-						print('----- toNextTrack_loop -----')
-						print(ex)
-				toNextTrack.start()
+			duration = track['duration']
+			@tasks.loop(seconds=duration, count=2, reconnect=False)
+			async def toNextTrack():
+				try:
+					nonlocal currentTrackIndex, playlist
+			
+					if toNextTrack.current_loop != 0:
+						if currentTrackIndex >= len(playlist) - 1:
+							currentTrackIndex = -1
+						
+						currentTrackIndex = currentTrackIndex + 1
+						voice.stop()
+						await playTrack(ctx)
+				except Exception as ex:
+					print('----- toNextTrack_loop -----')
+					print(ex)
+			toNextTrack.start()
 		except Exception as ex:
 			print('----- playTrack -----')
 			print(ex)
 
 
+
+	######################## PLAYLIST ########################
+	@slash.slash(name = "playlist", description = "Play next track", guild_ids = [guildId])
+	async def _playlist(ctx):
+		try:
+			nonlocal playlist
+			value = ""
+			# for track in playlist:
+			if len(playlist):
+				for i in range(len(playlist)):
+					track = playlist[i]
+					value += f"**{i}**・{track['title']}\n"
+				embed = discord.Embed(title='TeaBot', description="", color=0x1da1f2)
+				embed.add_field(name="📋・Playlist", value=value, inline=True)
+				await ctx.send(embed=embed)
+			else:
+				await ctx.send('Empty list')
+
+		except Exception as ex:
+			print('----- /playlist -----')
+			raise ex
+			print(ex)
+
 	######################## NEXT ########################
-	@slash.slash(name = "next", description = "Play next audio", guild_ids = [guildId])
+	@slash.slash(name = "next", description = "Play next track", guild_ids = [guildId])
 	async def next(ctx):
 		try:
 			nonlocal currentTrackIndex, playlist, ydl_opts
+			print(currentTrackIndex)
 			if len(playlist) == 0:
 				await ctx.send('The playlist is empty ⚠')
 				return
-			
+			await ctx.send('⏭ Next ...')
 			currentTrackIndex = currentTrackIndex + 1
 			if currentTrackIndex >= len(playlist):
 				currentTrackIndex = 0
 			voice = get(client.voice_clients, guild = ctx.guild)
 			voice.stop()
-			await playTrack(ctx, currentTrackIndex, voice, ydl_opts)
+			await playTrack(ctx)
 
 		except Exception as ex:
 			print('----- /next -----')
 			print(ex)
 
 	######################## PREVIOUS ########################
-	@slash.slash(name = "previous", description = "Play previous audio", guild_ids = [guildId])
+	@slash.slash(name = "previous", description = "Play previous track", guild_ids = [guildId])
 	async def previous(ctx):
 		try:
 			nonlocal currentTrackIndex, playlist, ydl_opts
+			print(currentTrackIndex)
 			if len(playlist) == 0:
 				await ctx.send('The playlist is empty ⚠')
 				return
-			
+			await ctx.send('⏮ Previous ...')
 			currentTrackIndex = currentTrackIndex - 1
 			if currentTrackIndex < 0:
 				currentTrackIndex = len(playlist) - 1
 			voice = get(client.voice_clients, guild = ctx.guild)
 			voice.stop()
-			await playTrack(ctx, currentTrackIndex, voice, ydl_opts)
+			await playTrack(ctx)
 
 		except Exception as ex:
 			print('----- /previous -----')
@@ -184,7 +233,7 @@ def init_audio_activity(params):
 				if voice.is_playing() or voice.is_connected():
 					await ctx.send('⏹ Stopping ...')
 					voice.stop()
-					await voice.disconnect()
+				await voice.disconnect()
 			else:
 				await ctx.send('❌ The bot is not playing anything at the moment')
 		except Exception as ex:
