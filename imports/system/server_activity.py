3a8ed1630c9b48d854412d3e31a31b11da423dc4
drissboumlik <idrissboumlik@gmail.com>
2021-11-07 02:21:46 +0100

    Make webhook to welcome user

--- imports/system/server_activity.py
+++ imports/system/server_activity.py
@@ -7,6 +7,7 @@ def init_server_activity(params):
 	discord = params['discord']
 	bot = params['bot']
 	slash = params['slash']
+	tasks = params['tasks']
 	get = params['get']
 	
 
@@ -40,26 +41,18 @@ def init_server_activity(params):
 				memberType = 'Human'
 			else:
 				memberType = 'Bot'
-			message = await welcomeMember(member)
+			message = await welcomeMember(member, 1)
 			if (message == -1):
 				msg = f'❗ DM/ Welcome Message ➜ **{member.name}#{member.discriminator} ({memberType})**'
 			else:
 				msg = f'📨 DM/ Welcome Message ➜ **{member.name}#{member.discriminator} ({memberType})**'
-			membersCount = await updateMembersCount(member, bot)
+			membersCount = await updateMembersCount(member)
 			channel = bot.get_channel(textChannels['log-server'])
 			msg += f'\n:green_square: **{membersCount}** - {member.mention} | [{member.name}#{member.discriminator}] | ({member.display_name}) join **TeaCode**'
 			await channel.send(msg)
 		except Exception as ex:
 			print('----- on_member_join 1-----')
 			print(ex)
-	
-		# try:
-		# 	webhook = await channel.create_webhook(name=member.name)
-		# 	await webhook.send(f'Hi I\'m {member.display_name}', username=member.name, avatar_url=member.avatar_url)
-		# 	await webhook.delete()
-		# except Exception as ex:
-		# 	print('----- on_member_join 3-----')
-		# 	print(ex)
 
 		try:
 			_roles = [
@@ -74,8 +67,14 @@ def init_server_activity(params):
 				roles_list.append(role)
 			await member.add_roles(*roles_list)
 			# time.sleep(5)
-			# role = get(guild.roles, id = roles['members'])
-			# await member.add_roles(role)
+
+			@tasks.loop(minutes=10, count=2, reconnect=True)
+			async def give_role_member():
+				if give_role_member.current_loop != 0:
+					role = get(guild.roles, id = roles['members'])
+					await member.add_roles(role)
+			give_role_member.start()
+
 		except Exception as ex:
 			print('----- on_member_join 2-----')
 			print(ex)
@@ -85,7 +84,7 @@ def init_server_activity(params):
 	@bot.event
 	async def on_member_remove(member):
 		try:
-			membersCount = await updateMembersCount(member, bot)
+			membersCount = await updateMembersCount(member)
 			channel = bot.get_channel(textChannels['log-server'])
 			msg = f':red_square: **{membersCount}** - {member.mention} | [{member.name}#{member.discriminator}] | ({member.display_name}) left **TeaCode**'
 			await channel.send(msg)
@@ -97,18 +96,19 @@ def init_server_activity(params):
 	# @bot.command(name="welcome", pass_context=True)
 	@slash.slash(name="welcome", guild_ids=[guildId],
 		permissions={ guildId: slash_permissions({'founders'}, {'members', 'everyone'}) })
-	async def welcome(ctx, member: discord.Member):
+	async def welcome(ctx, member: discord.Member, use_webhook: int=0):
 		try:
 			if not is_founders(ctx):
 				await ctx.send('❌ Missing Permissions')
 				return
 			await ctx.send(f'Msg sent to {member.mention}', hidden=True)
-			message = await welcomeMember(member)
+			message = await welcomeMember(member, )
+
 			if (message == -1):
 				msg = f'❗ DM/ Welcome Message ➜ **{member.name}#{member.discriminator}**'
 			else:
 				msg = f'📨 DM/ Welcome Message ➜ **{member.name}#{member.discriminator}**'
-			membersCount = await updateMembersCount(member, bot)
+			membersCount = await updateMembersCount(member)
 			channel = bot.get_channel(textChannels['log-server'])
 			msg += f'\n:green_square: **{membersCount}** - {member.mention} | [{member.name}#{member.discriminator}] | ({member.display_name}) join **TeaCode**'
 			await channel.send(msg)
@@ -116,55 +116,66 @@ def init_server_activity(params):
 			print('----- /welcome -----')
 			print(ex)
 
-######################## WELCOME MEMBER ########################
-async def welcomeMember(member):
-	try:
-		message = f'Merhba {member.mention} bik m3ana f **TeaCode Community** :partying_face: :tada: '
-		message += "\nWhere We help/support **Moroccans** :flag_ma: programming beginners :computer: in their learning journey :rocket:"
-		
-		message += "\n\nFollow the steps to **know** the rules & **see** the hidden rooms :warning:"
-		message += "\nIf you're **new** to discord check this video https://teacode.ma/about :bangbang:"
-		message += "\nDon't forget to **invite** your friends who could be interested :speech_left:"
+	######################## WELCOME MEMBER ########################
+	async def welcomeMember(member, use_webhook = 0):
+		try:
+			
+			if int(use_webhook):
+				try:
+					channel = bot.get_channel(textChannels['log-server'])
+					webhook = await channel.create_webhook(name=member.name)
+					await webhook.send(f'Hi I\'m {member.display_name}', username=member.name, avatar_url=member.avatar_url)
+					await webhook.delete()
+				except Exception as ex:
+					print('----- /welcome 2-----')
+					print(ex)
 
-		message += "\n\n__**0┊Activate your membership**__"
-		message += f"\n   **・**Read & React to the <#{textChannels['rules']}> to be a verified __**@Members**__."
-		
-		message += "\n\n__**1┊Go to**__"
-		message += f"\n   **・**<#{textChannels['get-roles']}> and react to get your skills roles."
-		message += f"\n   **・**<#{textChannels['faqs']}> where you can find answers to common questions about the server."
-		message += f"\n   **・**<#{textChannels['introduce-yourself']}> if you want to Introduce yourself . (First name, age, school .... etc)."
+			message = f'Merhba {member.mention} bik m3ana f **TeaCode Community** :partying_face: :tada: '
+			message += "\nWhere We help/support **Moroccans** :flag_ma: programming beginners :computer: in their learning journey :rocket:"
 
-		message += "\n\n__**2┊Need Help ?**__"
-		message += f"\n   **・**Ask the __**@Moderators**__/__**@Staff**__ guys here <#{textChannels['ask-staff']}> if you need help with the server."
+			message += "\n\nFollow the steps to **know** the rules & **see** the hidden rooms :warning:"
+			message += "\nIf you're **new** to discord check this video https://teacode.ma/about :bangbang:"
+			message += "\nDon't forget to **invite** your friends who could be interested :speech_left:"
 
-		message += "\n\n__**3┊Links**__"
-		message += "\n   **・**Website : <https://teacode.ma>"
-		message += "\n   **・**Discord : https://discord.gg/vKu2fkPqjY"
-		message += "\n   **・**Facebook : <https://teacode.ma/facebook>"
-		message += "\n   **・**YouTube : <https://teacode.ma/youtube>"
-		message += "\n   **・**Instagram : <https://teacode.ma/instagram>"
-		message += "\n   **・**Twitter : <https://teacode.ma/twitter>"
+			message += "\n\n__**0┊Activate your membership**__"
+			message += f"\n   **・**Read & React to the <#{textChannels['rules']}> to be a verified __**@Members**__."
 
-		message += "\n\n__**4┊How To Support us**__"
-		message += "\n   **・**PayPal : <https://teacode.ma/paypal>"
-		message += "\n   **・**Patreon : <https://teacode.ma/patreon>"
-	
-		channel = await member.create_dm()
-		await channel.send(message)
-		return message
-	except Exception as ex:
-		print('----- welcomeMember -----')
-		print(ex)
-		return -1
+			message += "\n\n__**1┊Go to**__"
+			message += f"\n   **・**<#{textChannels['get-roles']}> and react to get your skills roles."
+			message += f"\n   **・**<#{textChannels['faqs']}> where you can find answers to common questions about the server."
+			message += f"\n   **・**<#{textChannels['introduce-yourself']}> if you want to Introduce yourself . (First name, age, school .... etc)."
+
+			message += "\n\n__**2┊Need Help ?**__"
+			message += f"\n   **・**Ask the __**@Moderators**__/__**@Staff**__ guys here <#{textChannels['ask-staff']}> if you need help with the server."
+
+			message += "\n\n__**3┊Links**__"
+			message += "\n   **・**Website : <https://teacode.ma>"
+			message += "\n   **・**Discord : https://discord.gg/vKu2fkPqjY"
+			message += "\n   **・**Facebook : <https://teacode.ma/facebook>"
+			message += "\n   **・**YouTube : <https://teacode.ma/youtube>"
+			message += "\n   **・**Instagram : <https://teacode.ma/instagram>"
+			message += "\n   **・**Twitter : <https://teacode.ma/twitter>"
+
+			message += "\n\n__**4┊How To Support us**__"
+			message += "\n   **・**PayPal : <https://teacode.ma/paypal>"
+			message += "\n   **・**Patreon : <https://teacode.ma/patreon>"
+
+			channel = await member.create_dm()
+			await channel.send(message)
+			return message
+		except Exception as ex:
+			print('----- welcomeMember -----')
+			print(ex)
+			return -1
 
 
-######################## UPDATE MEMBERS COUNT ########################
-async def updateMembersCount(member, bot):
-	try:
-		guild = bot.get_guild(guildId)
-		memberList = guild.members
-		membersCount = len(memberList)
-		return membersCount
-	except Exception as ex:
-		print('----- updateMembersCount -----')
-		print(ex)
+	######################## UPDATE MEMBERS COUNT ########################
+	async def updateMembersCount(member):
+		try:
+			guild = bot.get_guild(guildId)
+			memberList = guild.members
+			membersCount = len(memberList)
+			return membersCount
+		except Exception as ex:
+			print('----- updateMembersCount -----')
+			print(ex)
