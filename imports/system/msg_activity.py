a544258e740b0378732e9bc28e946b0bcfd45146
drissboumlik <idrissboumlik@gmail.com>
2021-12-27 19:26:48 +0100

    Refactor msg activity

--- imports/system/msg_activity.py
+++ imports/system/msg_activity.py
@@ -9,79 +9,81 @@ def init_msg_activity(params):
 	######################## ON MESSAGE ########################
 	@bot.event
 	async def on_message(message):
-		try:
-			excludedCategories = [
-				categories['system-corner']
-			]
-
+		excludedCategories = [
+			categories['system-corner']
+		]
+		if message.channel.category_id not in excludedCategories:
 			try:
-				content = message.content
-				if str(message.channel.type) == 'text' and message.channel.category_id not in excludedCategories:
-					if content.count('@everyone') or content.count('@here'):
-						channel = message.channel
-						msg = 'Dont mention __everyone__ or __here__ please\n*Your message will be deleted after 5 seconds !!*'
-						await channel.send(msg, delete_after = 10)
-						await message.delete(delay=10)
-						return
-
-					blocked = [
-						'dlscord', 'discordnitro', 'discordglfts',
-						'discord-airdrop', 'discocrd-nitro'
-					]
-					spam = False
-					for b in blocked:
-						if message.content.count(b) > 0:
-							print('spam message')
-							await message.delete()
-							spam = True
-					if spam:
-						return
+				if await prohibited_mentions(message):
+					return
+				if await check_spam(message):
+					return
 			except Exception as ex:
 				print('----- on_message(evt)/everyone|spam -----')
 				print(ex)
+				await log_exception(ex, 'on_message(evt)/everyone|spam', None, bot)
+			try:
+				await log_member_dms(message)
+				await bot.process_commands(message)
+			except Exception as ex:
+				print('----- on_message(evt) -----')
+				print(ex)
+				await log_exception(ex, 'on_message(evt)/log_dms', None, bot)
 
-			if str(message.channel.type) == 'private':
-				author = message.author
-				# Testing
-				if author.id == users['drissboumlik']:
-					channel = author.dm_channel
-					await channel.send('am alive')
-					return
-				excludedIDs = [
-						users['drissboumlik'],
-						users['teabot'],
-					]
-				if (author.id not in excludedIDs):
-					msg = '──────────────────────'
-					msg += f'\nDM/ ◁='
-					msg += f'\n__From__\n{author} - {author.mention}'
-					msg += f'\n__Content__\n{"--Sticker--" if (message.content == "") else message.content}'
-					msg += get_attachments(message)
-					msg += get_embeds(message)
-					channel = bot.get_channel(textChannels['log-channel'])
-					await channel.send(msg)
-			# else:
-			# 	author = message.author
-			# 	if author.id == users['drissboumlik']:
-			# 		print(message.content)
-			await bot.process_commands(message)
-		except Exception as ex:
-			print('----- on_message(evt) -----')
-			print(ex)
+	async def log_member_dms(message):
+		if str(message.channel.type) == 'private':
+			author = message.author
+			# Testing
+			if author.id == users['drissboumlik']:
+				channel = author.dm_channel
+				await channel.send('am alive')
+				return
+			excludedIDs = [
+					users['drissboumlik'],
+					users['teabot'],
+				]
+			if (author.id not in excludedIDs):
+				msg = '──────────────────────'
+				msg += f'\nDM/ ◁='
+				msg += f'\n__From__\n{author} - {author.mention}'
+				msg += f'\n__Content__\n{"--Sticker--" if (message.content == "") else message.content}'
+				msg += get_attachments(message)
+				msg += get_embeds(message)
+				channel = bot.get_channel(textChannels['log-channel'])
+				await channel.send(msg)
 
+	async def prohibited_mentions(message):
+		content = message.content
+		if str(message.channel.type) == 'text':
+			if content.count('@everyone') or content.count('@here'):
+				channel = message.channel
+				msg = 'Dont mention __everyone__ or __here__ please\n*Your message will be deleted after 5 seconds !!*'
+				await channel.send(msg, delete_after = 10)
+				await message.delete(delay=10)
+				return True
+		return False
+	
+	async def check_spam(message):
+		blocked = [
+			'dlscord', 'discordnitro', 'discordglfts',
+			'discord-airdrop', 'discocrd-nitro'
+		]
+		spam = False
+		for b in blocked:
+			if message.content.count(b) > 0:
+				print('spam message')
+				await message.delete()
+				spam = True
+		return spam
 
 	######################## ON MESSAGE DELETE ########################
 	@bot.event
 	async def on_message_delete(message):
 		try:
-			excludedAuthors = [
-				# users['drissboumlik']
-				# users['teacode'],
-				# users['teabot'],
+			excludedCategories = [
+				categories['system-corner']
 			]
-			
-			messageAuthorId = message.author.id
-			if messageAuthorId not in excludedAuthors:
+			if message.channel.category_id not in excludedCategories:
 				msg = '──────────────────────'
 				msg += f'\n🗑 by {message.author.mention} in {message.channel.mention}'
 				
@@ -101,23 +103,20 @@ def init_msg_activity(params):
 		except Exception as ex:
 			print('----- on_message_delete(evt) -----')
 			print(ex)
+			await log_exception(ex, 'on_message_delete(evt)', None, bot)
 
 
 	######################## ON MESSAGE EDIT ########################
 	@bot.event
 	async def on_message_edit(before, after):
 		try:
-			excludedAuthors = [
-				# users['drissboumlik']
-				# users['teacode'],
-				# users['teabot'],
+			excludedCategories = [
+				categories['system-corner']
 			]
 
-			if (before.content.lower() == after.content.lower()):
-				return
-
-			messageAuthorId = before.author.id
-			if messageAuthorId not in excludedAuthors:
+			if before.channel.category_id not in excludedCategories:
+				if (before.content.lower() == after.content.lower()):
+					return
 				msg = '──────────────────────'
 				msg += f'\n✏ by {before.author.mention} in {before.channel.mention}'
 				
@@ -138,3 +137,4 @@ def init_msg_activity(params):
 		except Exception as ex:
 			print('----- on_message_edit(evt) -----')
 			print(ex)
+			await log_exception(ex, 'on_message_edit(evt)', None, bot)
