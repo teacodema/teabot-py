6f3cbb48358250ba11a844ba26bc029a21ed2b08
Driss <drissboumlik.dev@gmail.com>
2022-06-05 14:26:05 +0100

    Handle non existing categories

--- imports/system/voice_activity.py
+++ imports/system/voice_activity.py
@@ -7,30 +7,32 @@ def init_voice_activity(params):
 	tasks = params['tasks']
 
 	async def logVoice(guild, member, voice, joining, role_toggle_fct, voice1 = None, voice2 = None):
-		category = voice_data[voice.channel.category_id]
-		if category.__contains__('vc-role'):
-			role = guild.get_role(category['vc-role'])
-			await role_toggle_fct(role)
-		channel = bot.get_channel(category['vc-text'])
-		if joining:
-			msg = f'{member.mention} joined __**{voice.channel.name}**__'
-			if (not voice1.channel and voice2.channel) or (voice1.channel and voice2.channel and voice1.channel.category_id != voice2.channel.category_id):
-				if member.voice and member.voice.self_deaf:
-					msg += f"\n_{member.display_name}_ won't be able to hear you now 🔇"
-		else:
-			msg = f'{member.mention} left __**{voice.channel.name}**__'
-		task_send_msg(channel, msg) # await channel.send(msg)
+		if voice.channel and voice_data.__contains__(voice.channel.category_id):
+			category = voice_data[voice.channel.category_id]
+			if category.__contains__('vc-role'):
+				role = guild.get_role(category['vc-role'])
+				await role_toggle_fct(role)
+			channel = bot.get_channel(category['vc-text'])
+			if joining:
+				msg = f'{member.mention} joined __**{voice.channel.name}**__'
+				if (not voice1.channel and voice2.channel) or (voice1.channel and voice2.channel and voice1.channel.category_id != voice2.channel.category_id):
+					if member.voice and member.voice.self_deaf:
+						msg += f"\n_{member.display_name}_ won't be able to hear you now 🔇"
+			else:
+				msg = f'{member.mention} left __**{voice.channel.name}**__'
+			task_send_msg(channel, msg) # await channel.send(msg)
 
 	async def check_deafen(member, voice1, voice2):
-		if voice1.channel and voice2.channel:
-			if voice1.self_deaf != voice2.self_deaf:
-				if not voice2.self_deaf:
-					msg = f"_{member.display_name}_ is able to hear you now 🔉"
-				elif voice2.self_deaf:
-					msg = f"_{member.display_name}_ won't be able to hear you now 🔇"
-				category = voice_data[voice2.channel.category_id]
-				channel = bot.get_channel(category['vc-text'])
-				await channel.send(msg)
+		if voice2.channel and voice_data.__contains__(voice2.channel.category_id):
+			if voice1.channel and voice2.channel:
+				if voice1.self_deaf != voice2.self_deaf:
+					if not voice2.self_deaf:
+						msg = f"_{member.display_name}_ is able to hear you now 🔉"
+					elif voice2.self_deaf:
+						msg = f"_{member.display_name}_ won't be able to hear you now 🔇"
+					category = voice_data[voice2.channel.category_id]
+					channel = bot.get_channel(category['vc-text'])
+					await channel.send(msg)
 
 					
 	######################## VOICE ########################
