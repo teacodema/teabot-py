4fe2571144c9412f6268b77c59e3b787993e9c6d
Driss <drissboumlik.dev@gmail.com>
2022-06-10 16:16:53 +0100

    Remove extra line break

--- imports/member/tag_rules.py
+++ imports/member/tag_rules.py
@@ -13,7 +13,7 @@ def init_rules_tag(params):
 		try:
 			rule = next(item for item in rules if item["key"] == query)
 			rule_index = rules.index(rule) + 1
-			msg = f'**{rule_index} -** {rule["value"]}\n\n➜ Check <#{textChannels["rules"]}> or <https://teacode.ma/rules> for more details.'
+			msg = f'**{rule_index} -** {rule["value"]}\n➜ Check <#{textChannels["rules"]}> or <https://teacode.ma/rules> for more details.'
 			await interaction.send(msg)
 		except Exception as ex:
 			print('----- /tag_rules() -----')
