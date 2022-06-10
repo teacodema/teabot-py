445d751e686def247f0502fa3aca0bee7660d551
Driss <drissboumlik.dev@gmail.com>
2022-06-10 16:27:25 +0100

    Edit rules keys

--- setup/rules.py
+++ setup/rules.py
@@ -2,43 +2,43 @@ from setup.properties import *
 
 rules = [
     {
-        "key": "1-terms",
+        "key": "1 - Guidelines & Terms",
         "value": "Follow the Discord Community Guidelines (<https://discord.com/guidelines>) and Terms (<https://discord.com/terms>)",
     },
     {
-        "key": "2-behave",
+        "key": "2 - Behavior",
         "value": "Be kind, Be respectful, Be considerate, Treat everyone with respect, think about how your contribution will affect others in the community, for more details read this <https://conversation.guide>",
     },
     {
-        "key": "3-interaction",
+        "key": "3 - Interaction",
         "value": "When you join any voice/text channel, please avoid saying/writing any obscene content (spam, NSFW, ..etc) or bad words (directly or indirectly), otherwise you risk getting kicked/banned from the server",
     },
     {
-        "key": "4-profile",
+        "key": "4 - Profile",
         "value": "We keep the right to ask you to change your nickname/username/description if it's not readable or contains any bad words",
     },
     {
-        "key": "5-cheating",
+        "key": "5 - Cheating",
         "value": "Cheating in school related stuff (exams, projects .. etc) is prohibited",
     },
     {
-        "key": "6-community",
+        "key": "6 - Contribute",
         "value": f"If you see something against the rules or something that makes you feel unsafe, let the <@&{roles['staff']}> know. We want this server to be a welcoming space!",
     },
     {
-        "key": "7-help-guide",
+        "key": "7 - Help Guide",
         "value": f"Check for the Help Guide here <#{textChannels['read-me']}>",
     },
     {
-        "key": "8-server-guide",
+        "key": "8 - Server Guide",
         "value": f"Check for FAQ (Frequently Asked Questions) here <#{textChannels['faqs']}>",
     },
     {
-        "key": "9-no-promo",
+        "key": "9 - No promo",
         "value": f"No spam or self-promotion (server invites, advertisements, etc) without permission from a <@&{roles['staff']}>. This includes DMing fellow members",
     },
     {
-        "key": "10-no-nsfw",
+        "key": "10 - No NSFW",
         "value": "No NSFW or obscene content. This includes text, images, or links featuring nudity, sex, hard violence, or other graphically disturbing content"
     },
 ]
