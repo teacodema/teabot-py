e4ef8823a283be2eed0880ca1ca10ea9b2fa1438
Driss <drissboumlik.dev@gmail.com>
2022-06-25 00:00:08 +0100

    Reset bot status

--- imports/events/voice.py
+++ imports/events/voice.py
@@ -10,6 +10,8 @@ def init_events_voice(params):
 	@bot.event
 	async def on_voice_state_update(member, voice1, voice2):
 		try:
+			if voice2.channel == None:
+				task_update_activity(params)
 			await check_deafen(params, member, voice1, voice2)
 			if (voice1 and voice2) and (voice1.channel and voice2.channel) and (voice1.channel.id == voice2.channel.id):
 				return
@@ -18,7 +20,7 @@ def init_events_voice(params):
 				await logVoice(params, guild, member, voice1, False, member.remove_roles)
 			if voice2 and voice2.channel:
 				await logVoice(params, guild, member, voice2, True, member.add_roles, voice1, voice2)
-
+			
 			# voice_state = member.guild.voice_client
 			# if voice_state and len(voice_state.channel.members) == 1:
 			# 	await voice_state.disconnect()
