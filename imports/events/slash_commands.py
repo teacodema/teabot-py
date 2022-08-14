95583a2d6bf1c0183cdafd1fbfe570bcb4784605
Driss <drissboumlik.dev@gmail.com>
2022-08-14 18:29:11 +0100

    Add id mention

--- imports/events/slash_commands.py
+++ imports/events/slash_commands.py
@@ -1,3 +1,4 @@
+from setup.actions.message import *
 from setup.data.properties import *
 
 def init_slash_commands_events(params):
@@ -6,23 +7,24 @@ def init_slash_commands_events(params):
 
 	@bot.listen()
 	async def on_slash_command(inter):
-		msg = '-------on_slash_command-------'
+		msg = '------- on_slash_command -------'
 		msg += f'\nCommand  : {inter.application_command.name} / {inter.application_command.qualified_name}'
 		msg += f'\nChannel : {inter.channel.mention}'
-		msg += f'\nAuthor : {inter.author.mention} / {inter.author.id}'
+		user_mention = toggle_mention(inter.author, roles['root'], True)
+		msg += f'\nAuthor : {user_mention}'
 		channel = bot.get_channel(textChannels['log-cmd'])
 		await channel.send(msg)
 
 	@bot.listen()
 	async def on_slash_command_completion(inter):
-		msg = '-------on_slash_command_completion-------'
+		msg = '------- on_slash_command_completion -------'
 		msg += '\nCompleted'
 		channel = bot.get_channel(textChannels['log-cmd'])
 		await channel.send(msg)
 		
 	@bot.listen()
 	async def on_slash_command_error(inter, exception):
-		msg = '-------on_slash_command_error-------'
+		msg = '------- on_slash_command_error -------'
 		msg += '\nError'
 		msg += f'\n{str(exception)}'
 		channel = bot.get_channel(textChannels['log-cmd'])
--- setup/actions/message.py
+++ setup/actions/message.py
@@ -3,11 +3,12 @@ from setup.data.properties import *
 from setup.actions.common import *
 
 
-def toggle_mention(member, roleId):
+def toggle_mention(member, roleId, append_member_id = False):
 	user_mention = member.mention
 	role = member.guild.get_role(roleId)
 	if role in member.roles:
 		user_mention = f'{member.display_name}#{member.discriminator}'
+	if append_member_id: user_mention += f' / {member.id}'
 	return user_mention
 
 def get_attachments(message):
