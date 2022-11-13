20d76be19dcca6094a0ea93fdda7ad057c45b791
Driss <drissboumlik.dev@gmail.com>
2022-11-14 00:28:18 +0100

    Fix tabs

--- imports/slash_commands/voice.py
+++ imports/slash_commands/voice.py
@@ -8,20 +8,20 @@ def init_slash_commands_voice(params):
     
 	@bot.slash_command(name = "clone-vc")
 	async def clone_voice_channel(interaction, channel:discord.VoiceChannel, name = None):
-        """
-        Clone a voice channel
+		"""
+		Clone a voice channel
 		Parameters
 		----------
 		channel: Voice channel to clone
 		name: Channel name (optional)
 		"""
-        try:
+		try:
 			action_name = inspect.stack()[0][3]
 			if not is_allowed(interaction, action_name):
 				await interaction.send('❌ Missing Permissions', ephemeral=True)
 				return
-            await channel.clone(name = name)
-        except Exception as ex:
+			await channel.clone(name = name)
+		except Exception as ex:
 			print('----- /clone_voice_channel() -----')
 			print(ex)
 			await log_exception(ex, '/clone_voice_channel', interaction)
