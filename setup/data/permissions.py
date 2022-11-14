af739347d24178f7f379a1c2f7d209144857bd70
Driss <drissboumlik.dev@gmail.com>
2022-11-14 21:30:41 +0100

    Update permissions list

--- setup/data/permissions.py
+++ setup/data/permissions.py
@@ -5,14 +5,20 @@ functions_roles = {
         'track', 'queue', 'clear-queue', 'refresh', 'leave',
     ],
     'staff': [
-        'purge', 'event_create', 'clone_voice_channel', 'delete_voice_channel'
+        'purge',
+        'check_new_members', 'update_new_members',
+        'toggle_role', 'toggle_role_members',
+        'event_subscribers', 'event_create',
+        'clone_voice_channel', 'delete_voice_channel'
     ],
 	'root': [
         'server_info', 'role_info', 'member_info',
         'welcome', 'check_new_members', 'update_new_members',
         'make_webhook', 'msg_member', 'msg_channel', 'edit_msg_channel',
-        'reply_channel', 'remove_msg_member', 'purge', 'tag',
-        'bot_react', 'toggle_role', 'toggle_role_members', 'members_has_role', 'update_roles',
+        'reply_channel', 'remove_msg_member', 'purge',
+        'bot_react', 'get_message_reactions',
+        'toggle_role', 'toggle_role_members', 'members_has_role', 'update_roles',
         'event_subscribers', 'event_delete_between_dates', 'event_create',
+        'clone_voice_channel', 'delete_voice_channel',
     ],
 }
