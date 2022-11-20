c2ddcdfd3d0a7ae3982e1e52c2001bddb02b3353
Driss <drissboumlik.dev@gmail.com>
2022-11-20 01:36:13 +0100

    Add hosts special permissions

--- setup/data/permissions.py
+++ setup/data/permissions.py
@@ -1,25 +1,28 @@
 functions_roles = {
     '*': [
         'server_info', 'role_info', 'member_info',
-        'janken', 'pick_speaker',
+        'janken',
         'tag',
         'play', 'replay', 'pause', 'resume', 'previous', 'next', 'stop'
         'track', 'queue', 'clear-queue', 'refresh', 'leave',
     ],
+    'hosts': [
+        'pick_speaker'
+    ],
     'staff': [
         'purge',
+        'pick_speaker'
         'check_new_members', 'update_new_members',
         'toggle_role', 'toggle_role_members',
         'event_subscribers', 'event_create',
         'clone_voice_channel', 'delete_voice_channel'
     ],
 	'root': [
-        'welcome', 'check_new_members', 'update_new_members',
+        'welcome',
         'make_webhook', 'msg_member', 'msg_channel', 'edit_msg_channel',
-        'reply_channel', 'remove_msg_member', 'purge',
+        'reply_channel', 'remove_msg_member',
         'bot_react', 'get_message_reactions',
-        'toggle_role', 'toggle_role_members', 'members_has_role', 'update_roles',
-        'event_subscribers', 'event_delete_between_dates', 'event_create',
-        'clone_voice_channel', 'delete_voice_channel',
+        'members_has_role', 'update_roles',
+        'event_delete_between_dates',
     ],
 }
