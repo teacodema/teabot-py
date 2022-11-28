7c43ace05bae42205e0cb895ad4f71bb36f5895a
Driss <drissboumlik.dev@gmail.com>
2022-11-28 20:03:10 +0100

    Add desc

--- imports/slash_commands/thread.py
+++ imports/slash_commands/thread.py
@@ -7,10 +7,22 @@ def init_slash_commands_thread(params):
 	discord = params['discord']
 
 	@bot.slash_command(name = "tc_delete-threads")
-	async def delete_threads(interaction, channel: discord.abc.GuildChannel, delete_archived=None):
-		total_threads = channel.threads
-		if delete_archived:
-			total_threads = total_threads + await channel.archived_threads().flatten()
-		for thread in total_threads:
-			await thread.delete()
-				
+	async def delete_threads(interaction, channel: discord.abc.GuildChannel, delete_archived: int = 0):
+		"""
+		Delete Threads
+		Parameters
+		----------
+		channel: target channel
+		delete_archived: include archived threads - values 0/1
+		"""
+		try:
+			total_threads = channel.threads
+			if delete_archived:
+				total_threads = total_threads + await channel.archived_threads().flatten()
+			for thread in total_threads:
+				await thread.delete()
+		except Exception as ex:
+			print('----- /delete_threads() -----')
+			print(ex)
+			await log_exception(ex, '/delete_threads', interaction)
+
