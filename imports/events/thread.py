07453dc8b6ce99c7475f9dece41738f7c1365311
Driss <drissboumlik.dev@gmail.com>
2022-12-15 23:37:54 +0100

    Add thread events

---
+++ imports/events/thread.py
@@ -0,0 +1,28 @@
+
+from imports.actions.message import *
+from imports.actions.common import *
+
+def init_events_thread(params):
+
+    bot = params['bot']
+
+    @bot.event
+    async def on_raw_thread_delete(payload):
+        try:
+            thread = payload.thread
+            log = bot.get_channel(textChannels['log-txt'])
+            user_mention = await toggle_user_mention(bot, thread.owner, roles["mods"])
+            log_thread = await make_thread(log, f'🗑 Thread Deleted by {user_mention} in {toggle_channel_mention(thread.parent)}')
+
+            msg = '──────────────────────'
+            msg += f'\n🗑 by {user_mention} in {thread.parent.mention}'
+            msg += f'\nAuthor ID : {thread.owner_id}'
+            msg += f'\nThread : {thread.name}'
+            created_at = getTimeUtcPlusOne(thread.created_at, "%d %B %Y - %H:%M")
+            msg += f'\n📅 {created_at}'
+            await log_thread.send(msg.strip())
+            await log_thread.edit(archived=True)
+        except Exception as ex:
+            print('----- on_raw_thread_delete(evt) -----')
+            print(ex)
+            await log_exception(ex, 'on_raw_thread_delete(evt)', None, bot)
