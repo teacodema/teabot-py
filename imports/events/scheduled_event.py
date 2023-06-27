fc5a051caa248c943627b4e45e1408e8524afa16
Driss <drissboumlik.work@gmail.com>
2023-06-27 21:52:59 +0100

    Disable role adding on event subscribe

--- imports/events/scheduled_event.py
+++ imports/events/scheduled_event.py
@@ -6,19 +6,19 @@ def init_events_scheduled_event(params):
 	bot = params['bot']
 	discord = params['discord']
 	
-	@bot.event
-	async def on_raw_guild_scheduled_event_subscribe(payload):
-		try:
-			guild = bot.get_guild(guildId)
-			event = guild.get_scheduled_event(payload.event_id)
-			if event.channel_id in voice_roles:
-				member = await guild.fetch_member(payload.user_id)
-				role = guild.get_role(voice_roles[event.channel_id])
-				await member.add_roles(role)
-		except Exception as ex:
-			print('----- on_raw_guild_scheduled_event_subscribe(evt) -----')
-			print(ex)
-			await log_exception(ex, 'on_raw_guild_scheduled_event_subscribe(evt)', None, bot)
+	# @bot.event
+	# async def on_raw_guild_scheduled_event_subscribe(payload):
+	# 	try:
+	# 		guild = bot.get_guild(guildId)
+	# 		event = guild.get_scheduled_event(payload.event_id)
+	# 		if event.channel_id in voice_roles:
+	# 			member = await guild.fetch_member(payload.user_id)
+	# 			role = guild.get_role(voice_roles[event.channel_id])
+	# 			await member.add_roles(role)
+	# 	except Exception as ex:
+	# 		print('----- on_raw_guild_scheduled_event_subscribe(evt) -----')
+	# 		print(ex)
+	# 		await log_exception(ex, 'on_raw_guild_scheduled_event_subscribe(evt)', None, bot)
 			
 	@bot.event
 	async def on_guild_scheduled_event_update(before, after):
