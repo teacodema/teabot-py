27f4d0e6d7af0323d8c85e943d03c7605d7914d0
Driss <drissboumlik.dev@gmail.com>
2024-03-04 22:44:43 +0100

    Clean voice event code

--- imports/events/voice.py
+++ imports/events/voice.py
@@ -12,13 +12,15 @@ def init_events_voice(params):
 			if voice2.channel == None:
 				await task_update_activity(params)
 			await check_deafen(params, member, voice1, voice2)
-			if (voice1 and voice2) and (voice1.channel and voice2.channel) and (voice1.channel.id == voice2.channel.id):
+			if ((voice1 and voice2)
+	   			and (voice1.channel and voice2.channel)
+				and (voice1.channel.id == voice2.channel.id)):
 				return
 			guild = bot.get_guild(guildId)
 			if voice1 and voice1.channel:
-				await logVoice(params, guild, member, voice1, False, member.remove_roles)
+				await logVoice(params, guild, member, voice1, joining=False, role_toggle_fct=member.remove_roles)
 			if voice2 and voice2.channel:
-				await logVoice(params, guild, member, voice2, True, member.add_roles, voice1, voice2)
+				await logVoice(params, guild, member, voice2, joining=True, role_toggle_fct=member.add_roles, voice1=voice1, voice2=voice2)
 			
 			# voice_state = member.guild.voice_client
 			# if voice_state and len(voice_state.channel.members) == 1:
