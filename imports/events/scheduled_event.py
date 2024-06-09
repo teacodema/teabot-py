b36150abad35dd2b2ded659427fb26930bd2dfa0
Driss <drissboumlik.dev@gmail.com>
2024-06-09 15:45:56 +0100

    Edit acitivity on event update

--- imports/events/scheduled_event.py
+++ imports/events/scheduled_event.py
@@ -6,42 +6,42 @@ def init_events_scheduled_event(params):
 	bot = params['bot']
 	discord = params['discord']
 	
-	@bot.event
-	async def on_raw_guild_scheduled_event_subscribe(payload):
-		try:
-			guild = bot.get_guild(payload.guild_id)
-			event = guild.get_scheduled_event(payload.event_id)
-			msg = f'🔹 <@{payload.user_id}> subscribed to **{event.name}** / {event.id}'
-			channel = bot.get_channel(textChannels['log-event'])
-			await channel.send(msg)
-			# if event.channel_id in voice_roles:
-				# member = await guild.fetch_member(payload.user_id)
-				# role = guild.get_role(voice_roles[event.channel_id])
-				# await member.add_roles(role)
-		except Exception as ex:
-			print('----- on_raw_guild_scheduled_event_subscribe(evt) -----')
-			print(ex)
-			await log_exception(ex, 'on_raw_guild_scheduled_event_subscribe(evt)', None, bot)
+	# @bot.event
+	# async def on_raw_guild_scheduled_event_subscribe(payload):
+	# 	try:
+	# 		guild = bot.get_guild(payload.guild_id)
+	# 		event = guild.get_scheduled_event(payload.event_id)
+	# 		msg = f'🔹 <@{payload.user_id}> subscribed to **{event.name}** / {event.id}'
+	# 		channel = bot.get_channel(textChannels['log-event'])
+	# 		await channel.send(msg)
+	# 		# if event.channel_id in voice_roles:
+	# 			# member = await guild.fetch_member(payload.user_id)
+	# 			# role = guild.get_role(voice_roles[event.channel_id])
+	# 			# await member.add_roles(role)
+	# 	except Exception as ex:
+	# 		print('----- on_raw_guild_scheduled_event_subscribe(evt) -----')
+	# 		print(ex)
+	# 		await log_exception(ex, 'on_raw_guild_scheduled_event_subscribe(evt)', None, bot)
 
-	@bot.event
-	async def on_raw_guild_scheduled_event_unsubscribe(payload):
-		try:
-			guild = bot.get_guild(payload.guild_id)
-			event = guild.get_scheduled_event(payload.event_id)
-			msg = f'🔸 <@{payload.user_id}> unsubscribed from **{event.name}** / {event.id}'
-			channel = bot.get_channel(textChannels['log-event'])
-			await channel.send(msg)
-		except Exception as ex:
-			print('----- on_raw_guild_scheduled_event_unsubscribe(evt) -----')
-			print(ex)
-			await log_exception(ex, 'on_raw_guild_scheduled_event_unsubscribe(evt)', None, bot)
+	# @bot.event
+	# async def on_raw_guild_scheduled_event_unsubscribe(payload):
+	# 	try:
+	# 		guild = bot.get_guild(payload.guild_id)
+	# 		event = guild.get_scheduled_event(payload.event_id)
+	# 		msg = f'🔸 <@{payload.user_id}> unsubscribed from **{event.name}** / {event.id}'
+	# 		channel = bot.get_channel(textChannels['log-event'])
+	# 		await channel.send(msg)
+	# 	except Exception as ex:
+	# 		print('----- on_raw_guild_scheduled_event_unsubscribe(evt) -----')
+	# 		print(ex)
+	# 		await log_exception(ex, 'on_raw_guild_scheduled_event_unsubscribe(evt)', None, bot)
 			
 	@bot.event
 	async def on_guild_scheduled_event_update(before, after):
 		try:
 			# invite = await after.channel.create_invite(max_age=appParams['inviteForOneWeek'], max_uses=100, reason=f'Event Started Title : {after.name}')
 			# event_invite_link = f'{invite}?event={after.id}'
-			if before.status != after.status:
+			if after.channel.category_id == categories['voice-channels'] and before.status != after.status:
 				# channel = bot.get_channel(textChannels['general'])
 				# guild = bot.get_guild(guildId)
 				# if after.channel_id in voice_roles:
