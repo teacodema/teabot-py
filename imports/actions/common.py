e0400eda62adeb3d71ff0bb32758d04a9dd5b1f5
Driss <drissboumlik.dev@gmail.com>
2026-07-27 20:16:49 +0100

    Edit activity text

--- imports/actions/common.py
+++ imports/actions/common.py
@@ -103,7 +103,7 @@ def split_str(str, spliters=None):
 async def task_update_activity(params, activity_name = None, activity_type = None, status = None):
 	bot = params['bot']
 	discord = params['discord']
-	activity_names = ["teacode ☕", "server activity 🪔", "interview@drissboumlik.com", "community@drissboumlik.com"]
+	activity_names = ["Teacode ☕", "Server activity 🪔", "interview@drissboumlik.com", "community@drissboumlik.com"]
 	if status == None: status = discord.Status.online
 	if activity_type == None: activity_type = discord.ActivityType.watching
 	if activity_name == None: activity_name = random.choice(activity_names)
